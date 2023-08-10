4de90c2e009719dd3fcd2458809b9d2f51c631d0
andre <andremaivelasco@hotmail.com>
2023-08-09 21:02:04 -0300

    git aplicado e packages criados

---
+++ src/test/java/com/example/Lembretes/LembretesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lembretes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LembretesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
